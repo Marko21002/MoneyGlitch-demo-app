aa8ffba43f27c0bdaaa1fa08f1b7a0fec8831c8d
Marko21002 <markgufman21@gmail.com>
2024-07-08 21:03:32 +0200

    fixed ctx

--- src/convex/clerk.ts
+++ src/convex/clerk.ts
@@ -7,7 +7,8 @@ import { v } from "convex/values";
 
 export const fulfill = internalAction({
   args: { headers: v.any(), payload: v.string() },
-  handler: async (ctx, args) => {
+  handler: async (_, args) => {
+    // Removed `ctx`
     const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET as string);
     const payload = wh.verify(args.payload, args.headers) as WebhookEvent;
     return payload;
